dd7321ed4eb909d01bcc2294b2a7b9b8a7a2545d
Ali Ahmed <ali.ahmed@punchcard.io>
2020-08-16 15:57:16 -0600

    basic styling with tailwind - enabling basic sorting on the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { getInspections } from './api';
 import { Inspection } from './types';
-import { useTable, Column, HeaderGroup, Row, Cell } from 'react-table';
+import {
+	useTable,
+	Column,
+	HeaderGroup,
+	Row,
+	Cell,
+	useSortBy,
+} from 'react-table';
+import './tailwind.output.css';
 
 function App() {
 	const [loading, setLoading] = useState<boolean>(true);
@@ -20,10 +28,8 @@ function App() {
 
 	const columns = useMemo(
 		() => [
-			{
-				Header: 'Id',
-				accessor: 'id',
-			},
+			{ Header: '#', id: 'rowNumber', accessor: (_: any, i: number) => i },
+
 			{
 				Header: 'Customer Information',
 				columns: [
@@ -32,27 +38,41 @@ function App() {
 				],
 			},
 			{
-				Header: 'Reference #',
-				accessor: 'referenceNo.value',
+				Header: 'User Information',
+				columns: [
+					{
+						Header: 'User',
+						id: 'user',
+						accessor: (row: Inspection) =>
+							row.user!.firstName + ' ' + row.user!.lastName,
+					},
+				],
 			},
 			{
-				Header: 'User',
-				id: 'user',
-				accessor: (row: Inspection) =>
-					row.user!.firstName + ' ' + row.user!.lastName,
-			},
-			{
-				Header: 'Location',
-				accessor: 'location.name',
-			},
-			{
-				Header: 'Type',
-				accessor: 'type',
-			},
-			{
-				Header: 'Created At',
-				id: 'createdAt',
-				accessor: (row: any) => new Date(row.createdAt).toLocaleString(),
+				Header: 'Inspection Information',
+				columns: [
+					{
+						Header: 'Inspection Id',
+						accessor: 'id',
+					},
+					{
+						Header: 'Location',
+						accessor: 'location.name',
+					},
+					{
+						Header: 'Reference #',
+						accessor: 'referenceNo.value',
+					},
+					{
+						Header: 'Type',
+						accessor: 'type',
+					},
+					{
+						Header: 'Created At',
+						id: 'createdAt',
+						accessor: (row: any) => new Date(row.createdAt).toLocaleString(),
+					},
+				],
 			},
 		],
 		[]
@@ -65,7 +85,7 @@ function App() {
 		rows,
 		prepareRow,
 		// @ts-ignore
-	} = useTable({ columns, data: inspections });
+	} = useTable({ columns, data: inspections }, useSortBy);
 
 	if (loading) {
 		return (
@@ -76,13 +96,24 @@ function App() {
 	}
 
 	return (
-		<div className="App">
-			<table {...getTableProps()}>
-				<thead>
-					{headerGroups.map((headerGroup: HeaderGroup<Inspection>) => (
+		<div className="p-10 container mx-auto">
+			<table {...getTableProps()} className="mx-auto table">
+				<thead className="table-header-group">
+					{headerGroups.map((headerGroup: any) => (
 						<tr {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map((column: HeaderGroup<Inspection>) => (
-								<th {...column.getHeaderProps()}>{column.render('Header')}</th>
+							{headerGroup.headers.map((column: any) => (
+								<th
+									{...column.getHeaderProps(column.getSortByToggleProps())}
+									className="border-2 border-blue-400 bg-gray-300"
+								>
+									{column.render('Header')}
+
+									{column.isSorted
+										? column.isSortedDesc
+											? ' 🔽'
+											: ' 🔼'
+										: ''}
+								</th>
 							))}
 						</tr>
 					))}
@@ -91,9 +122,17 @@ function App() {
 					{rows.map((row: Row<Inspection>) => {
 						prepareRow(row);
 						return (
-							<tr {...row.getRowProps()}>
+							<tr
+								{...row.getRowProps()}
+								className="border-t last:border-b bg-red odd:bg-gray table-row"
+							>
 								{row.cells.map((cell: Cell<Inspection, any>) => (
-									<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+									<td
+										{...cell.getCellProps()}
+										className="border-t bg-red odd:bg-gray table-cell py-2 px-4"
+									>
+										{cell.render('Cell')}
+									</td>
 								))}
 							</tr>
 						);
